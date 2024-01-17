9c0f182b2dcd00451b4fd33fe4b1f264bde536cb
zijin <zijin345@126.com>
2024-01-17 13:49:50 +0800

    feat: 初始化项目

---
+++ utils/transformAmount.ts
@@ -0,0 +1,34 @@
+import Num from "number-precision"; // 保证精度不丢失
+
+/**
+ * 单位扩大两位，一般用于（分->元）（百分数->数）
+ * @param {*} amount 当前金额
+ * @param {*} accuracy 保留几位小数，默认2位
+ */
+// tag 记一下为啥有 accuracy 这个参数？
+export const sicExpand = (amount: number | string, accuracy = 2) => {
+  if (amount || amount === 0) {
+    if (accuracy < 0) {
+      const realAmount = Num.divide(amount, 100);
+      return realAmount;
+    } else {
+      const realAmount = Num.divide(amount, 100).toFixed(accuracy);
+      return realAmount;
+    }
+  } else {
+    return amount;
+  }
+};
+
+/**
+ * 单位缩小两位，一般用于（元->分）（数->百分数）
+ * @param {*} amount 当前金额
+ */
+export const sicReduce = (amount: any) => {
+  if (amount || amount === 0) {
+    const realAmount = Num.times(amount, 100);
+    return realAmount;
+  } else {
+    return amount;
+  }
+};
